fa42275da26c62c4d916cec06e1271a2f2130059
AeroDoesCoding <44150824+AeroDoesCoding@users.noreply.github.com>
2020-04-16 14:51:48 +0700

    Update emergency files

--- Eka's Function/In Case of Emergency/function.h
+++
@@ -1,6 +0,0 @@
-int check_cell(char* grid,int width,int height, int posX, int posY);
-void read_inputFile(char filename[50],char** grid, int* width, int* height);
-void delay_ms(int t);
-void print_grid(char* grid,int width,int height);
-void tick(char** grid,int width, int height);
-void animate(char** grid,int width,int height,int frame);
