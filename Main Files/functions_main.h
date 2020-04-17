41ffebb20d1958ac0bfba0fb75bdafb508274adf
AeroDoesCoding <44150824+AeroDoesCoding@users.noreply.github.com>
2020-04-17 16:14:00 +0700

    Clean-up main functions.h

--- Main Files/functions_main.h
+++ Main Files/functions_main.h
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+//Eka-------------------------------------------------------------------------------------------------------------------------------------
 //read_inputFile membaca file seed untuk Game of Life dan menaruhnya ke dalam sebuah array 1 dimensi dengan panjang dan lebarnya
 void read_inputFile(char filename[50], char** grid, int* width, int* height);
 /*Check_cell mengecek tetangga dari sebuah cell dengan posisi x,y dan menentukan apakah cell tersebut hidup atau mati di iterasi selanjutnya
   Return 1 jika hidup, return 0 jika mati*/
 int check_cell(char* grid,int width,int height, int posX, int posY);
-void delay(int delay_time_ms);
-void animate(int h, int w, int count, char** array);
+
+//Emergency
 void print_grid(char* grid,int width,int height);
 void tick(char** grid,int width, int height);
+
+//Cahya
+void delay(int delay_time_ms);
+void animate(int h, int w, int count, char** array);
 
